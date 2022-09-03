23cc1ea653b1ff5a82a7e7738d936d91450425a9
miniproyecto <yurimar2003@gmail.com>
2022-09-03 15:30:07 -0400

    cambio del encabezado

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Sistema Panificadora</title>
+  <title>Sistema Panificadora.</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="./view/vendors/feather/feather.css">
   <link rel="stylesheet" href="./view/vendors/ti-icons/css/themify-icons.css">
